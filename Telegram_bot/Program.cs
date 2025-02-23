52c4ea121db9be6d88c273f0f1a152d587852ea5
Bochman9 <97672160+Bochman9@users.noreply.github.com>
2025-02-23 21:35:48 +0500

    Add files via upload

---
+++ Telegram_bot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Telegram.Bot;
+
+
+namespace Telegram_bot
+{
+    class Program
+    {
+        private static string token { get; set; } = "7706907828:AAG3CzS48IDwnnVhX3ur_qBw_Ys3X2KgII8";
+        private static TelegramBotClient client;
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+        private static void OnMessageHandler(object sendler, MessageEvantArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+                Console.WriteLine($"Пришло сообщение с текстом: {msg.Text}");
+            }
+        }
+    }
+}
